df4ee293bf53d7429f84d007bff12064ca70ddeb
RuwanDilhara <roshansameera916@gmail.com>
2024-10-15 18:10:54 +0530

    third commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// =======================================LOGIN BY BUTTON===========================================
+
 function login() {
     let username = document.getElementById("txtusername").value;
     let password = document.getElementById("txtpassword").value;
@@ -5,8 +7,16 @@ function login() {
     if (username === 'admin@123' && password === '1234') {
         window.location.href = 'Admin/admin.html'
     }
-    
+
     else if (username === 'cashier@123' && password === '1234') {
         window.location.href = 'Cashier/cashier.html'
     }
 }
+
+// ======================================= LOGIN BY ENTER KEY ===========================================
+
+document.getElementById("txtpassword").addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    login();
+  }
+});
